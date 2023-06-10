b98b25fe7d4a2b5deea7bcf44da554a5af015984
matheusaraujo07 <122407467+matheusaraujo07@users.noreply.github.com>
2023-06-10 12:09:53 -0300

    Create app.js
    Aqui são chamadas todas as funções principais da págia web.

---
+++ app.js
@@ -0,0 +1,14 @@
+import Banner from './Componentes/Banner/banner';
+import Formulario from './Componentes/Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <Formulario/>
+    </div>
+  );
+}
+
+export default App;
+
