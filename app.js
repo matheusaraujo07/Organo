5e789d6d3c927f4b4016e7cab4655e01b0cefac5
matheusaraujo07 <matheusataujo07>
2023-06-12 18:43:05 -0300

    commit feito

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './Componentes/Banner/banner';
 import Formulario from './Componentes/Formulario';
 
 function App() {
+  
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorCadastrado = (colaborador) =>{
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+  
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado={colaborador=> aoNovoColaboradorCadastrado(colaborador)}/>
     </div>
   );
 }
